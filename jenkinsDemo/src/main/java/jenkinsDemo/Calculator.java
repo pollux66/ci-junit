e1f7136c36a1cfded7b2d690033cbf0695aca266
pollux66 <robert.bayani@ymail.com>
2021-02-09 14:56:17 +0800

    Update

--- jenkinsDemo/src/main/java/jenkinsDemo/Calculator.java
+++ jenkinsDemo/src/main/java/jenkinsDemo/Calculator.java
@@ -3,12 +3,7 @@ package jenkinsDemo;
 public class Calculator {
 
     public int multiply(int a, int b) {
-        if (a = 0  || b = 0 ) {
-            return 0;
-        }
-        else {
-            return a * b;
-        }
+	return a * b;
     }
 
     public int add (int a, int b) {
